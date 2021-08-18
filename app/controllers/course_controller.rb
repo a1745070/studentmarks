ca61afde73da878c4bc2d16408cc0eb440f2e956
Christopher Ho <christopher.ho@student.adelaide.edu.au>
2021-08-18 22:15:07 +0930

    controller structure

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,21 @@
 class CourseController < ApplicationController
+  def admin
+  end
+  
+  def new
+  end
+  
+  def user
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  
 end
