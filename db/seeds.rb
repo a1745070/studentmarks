89fdfac790657d5179fe852deeb7d6044355dc56
Peter Quoc Cuong Trinh <peter.trinh@student.adelaide.edu.au>
2021-08-19 23:35:57 +0930

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,32 +8,6 @@
 #
 
 student_list = [
-  [ "a1781637","ESASII","HD","95" ]
-
-]
-
-assignment_list = [
-  [  ]
-
-]
-
-student_list.each do |studentid, courseid, grade, mark|
-  StudentMark.create(studentid: studentid, courseid: courseid, grade: grade, mark: mark)
-end
-
-assignment_list.each do |studentid, courseid, grade, mark|
-  Assignment.create(studentid: studentid, courseid: courseid, grade: grade, mark: mark)
-end
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-#
-
-student_list = [
   ["a1781637","ESAASII","HD","95"],
   ["a1783331","ESAASII","D","70"],
   ["a1773884","ESAASII","P","51"],
