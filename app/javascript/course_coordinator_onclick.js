7cda0158a11e86f18e385cbadad59d8b5a6e77f8
Ian Wei Chong <a1784585@student.adelaide.edu.au>
2021-08-19 21:11:27 +0800

    basic js

---
+++ app/javascript/course_coordinator_onclick.js
@@ -0,0 +1,5 @@
+document.getElementById("course_coordinator_onclick").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("course_coordinator_onclick").innerHTML = "YOU CLICKED ME!";
+}
