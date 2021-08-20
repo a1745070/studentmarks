73ccb9ad1717269ab6b77c48f84925abcb8d5881
Bobby Yan <a1744633@student.adelaide.edu.au>
2021-08-20 10:45:05 +0930

    removed course_coordinator_onclick.js

--- app/javascript/course_coordinator_onclick.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("course_coordinator_onclick").addEventListener("click", myFunction);
-
-function myFunction() {
-    document.getElementById("course_coordinator_onclick").innerHTML = "YOU CLICKED ME!";
-}
