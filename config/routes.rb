49b8499ae70fe04da00495ba7304c80718d40d24
Christopher Ho <christopher.ho@student.adelaide.edu.au>
2021-08-23 22:07:23 +0930

    feedback form _  + feedback view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'assignment#admin'
   get '/assignment/student', to: "assignment#student"
+  get '/assignment/feedback', to: "assignment#feedback"
   # add routes later to the html.erb
   get '/assignment/admin' => 'assignment#admin'
   patch '/assignment/update' => 'assignment#update'
