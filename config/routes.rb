a24e4f07fc7b90be9160d3833a8bf8fd3151be7f
Christopher Ho <christopher.ho@student.adelaide.edu.au>
2021-08-23 22:00:59 +0930

    adding student view for feedback requesting
    feedback requesting in student view addition

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'assignment#admin'
+  get '/assignment/student', to: "assignment#student"
   # add routes later to the html.erb
   get '/assignment/admin' => 'assignment#admin'
   patch '/assignment/update' => 'assignment#update'
