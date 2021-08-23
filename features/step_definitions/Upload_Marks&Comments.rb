09132743880520d8af88ca11d54bad78f3fc27cd
Archie Verma <a1745070@student.adelaide.edu.au>
2021-08-23 22:42:17 +0930

    cucumber skeleton

---
+++ features/step_definitions/Upload_Marks&Comments.rb
@@ -0,0 +1,15 @@
+Given(/^I have students with studentids and courseids$/) do
+	pending 
+end
+
+When(/^I go to assessment marks$/) do
+	pending 
+end
+
+Then(/^I should be able to add a Mark$/) do
+	pending 
+end
+
+And(/^I should be able to add a Comment$/) do
+	pending 
+end
