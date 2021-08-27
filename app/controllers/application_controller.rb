fe9194a14a07c084e82cc0836563c942b8ed7e42
Bobby Yan <a1744633@student.adelaide.edu.au>
2021-08-28 00:42:24 +0930

    done the function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-
-
+  require 'csv'
+  include ActionController::MimeResponds
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,29 @@ class User < ApplicationRecord
   has_many :tasks
   has_many :enrolments
 
-  def self.func
-    @user = User.find_by_id(7)
-    puts "+++++++++++++++++++++++++++++++++++++++++++++"
-    puts @user.role[:role]
+  def self.to_csv
+    @all = Task.includes(:assignment , :user, :mark).all
+    @coursenames = Course.all
+    attributes = %w{studentid name coursename assignmentname mark grade}
+
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+
+      @all.each do |user|
+        @a1 = user.user.studentid
+        @a2 = user.user.name
+        @a3 = user.assignment.course_id
+        if @a3 == 1
+          @a3 = @coursenames.where(id: 1).pluck(:coursename)
+        end
+        if @a3 == 2
+          @a3 = @coursenames.where(id: 2).pluck(:coursename)
+        end
+        @a4 = user.assignment.assignmentname
+        @a5 = user.mark.mark
+        @a6 = user.mark.grade
+        csv << [@a1,@a2,@a3,@a4,@a5,@a6]
+      end
+    end
   end
 end
