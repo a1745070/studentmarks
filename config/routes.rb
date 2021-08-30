7ce1ae600c4c2c327476b48bf96f32c0622c1027
Archie Verma <a1745070@student.adelaide.edu.au>
2021-08-30 21:34:49 +0930

    Created assignments page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'assignments/assignments'
   get 'modules/modules'
   get 'annoucement/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
