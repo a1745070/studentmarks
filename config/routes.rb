d1730fb6a8fe0766d0b7acdd4f78196b7719b115
Archie Verma <a1745070@student.adelaide.edu.au>
2021-08-30 21:33:28 +0930

    Created modules page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'modules/modules'
   get 'annoucement/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'assignment#admin'
