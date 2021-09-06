2ac3d2fef2d313555e5e8a23921e59da578eba34
Spirit18 <a1767346@student.adelaide.edu.au>
2021-09-06 21:59:17 +0930

    craeted sad cases and background users

--- features/step_definitions/login.rb
+++ features/step_definitions/login.rb
@@ -5,3 +5,10 @@ end
 Then(/^I am on the student assignment page$/) do
   visit assignment_student_path
 end
+
+Given(/^the following users exist:$/) do |table|
+  # table is a table.hashes.keys # => [:studentid, :password]
+  table.hashes.each do |user|
+    User.create!(user)
+  end
+end
