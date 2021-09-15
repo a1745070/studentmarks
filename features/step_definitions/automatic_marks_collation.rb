5a8fad05dc852c5109e21d9a9de0906c836eb385
Spirit18 <a1767346@student.adelaide.edu.au>
2021-09-15 20:06:52 +0930

    stash copy

--- features/step_definitions/automatic_marks_collation.rb
+++ features/step_definitions/automatic_marks_collation.rb
@@ -1,3 +1,7 @@
 Then(/^The system should compile all "([^"]*)" from database into "([^"]*)"$/) do |arg1, arg2|
   pending
+  # table is a table.hashes.keys # => [:studentid, :password]
+  table.hashes.each do |user|
+    User.create!(user)
+  end
 end
